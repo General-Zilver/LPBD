723b0120797989eb770695def8e3b9b64de1e35e
Greenspore272 <adriangonzalez9801@gmail.com>
2026-02-17 23:49:53 -0600

    Selection page

--- start.py
+++ start.py
@@ -1,15 +1,19 @@
 import customtkinter as ctk
 from login import LoginPage
+from main import WelcomePage
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Student Benefit Analyzer")
-        self.geometry("600x500")
+        self.geometry("650x500")
 
         # Load login page
         login_page = LoginPage(self)
         login_page.pack(fill="both", expand=True)
+        # main_page = WelcomePage(self)
+        # main_page.pack(fill="both", expand=True)
+
 
 if __name__ == "__main__":
     app = App()
